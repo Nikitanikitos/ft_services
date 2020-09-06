56cf133834afafc66732b4d413239814b364940c
Nikita <nikitos_athletos@mail.ru>
2020-09-07 01:30:04 +0300

    added in setup.sh commands

--- setup.sh
+++ setup.sh
@@ -6,9 +6,15 @@
 #    By: imicah <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/05 20:24:16 by imicah            #+#    #+#              #
-#    Updated: 2020/09/06 15:41:51 by imicah           ###   ########.fr        #
+#    Updated: 2020/09/07 01:29:42 by imicah           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-docker build srcs/nginx/Dockerfile -t nginx-server
-docker run -p 80:80 -p 443:443 -p 2222:22 nginx-server
+docker build srcs/nginx/Dockerfile -t		nginx-server
+docker build srcs/phpmyadmin/Dockerfile -t	phpmyadmin
+docker build srcs/wordpress/Dockerfile -t	wordpress
+
+docker run -p 5050:5050 --name wordpress wordpress
+docker run -p 5000:5000 --name phpmyadmin phpmyadmin
+docker run -p 80:80 -p 443:443 -p 2222:22 --name nginx-server nginx-server
+
