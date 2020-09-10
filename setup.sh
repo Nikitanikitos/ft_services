593857ab17fa8283a0decd9f4177af42ee1ab71d
Irving Micah <imicah@va-g1.kzn.21-school.ru>
2020-09-10 21:07:53 +0300

    complited valumes in db

--- setup.sh
+++ setup.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+services=("nginx" "phpmyadmin" "wordpress" "mysql" "influx" "grafana")
+
 minikube start --vm-driver=virtualbox
-eval $(minikube docker-env) 
+eval $(minikube docker-env) > /dev/null
 
 minikube addons enable metallb
 kubectl apply -f srcs/minikube_confs/metallb-configmap.yaml
 
-#docker build srcs/nginx/ -t			nginx-image
-#docker build srcs/phpmyadmin/ -t	phpmyadmin-image
-docker build srcs/wordpress/ -t		wordpress-image
-docker build srcs/mysql -t			mysql-image
+for service in "${services[@]}"
+do
+printf "docker build ${service}: "
+docker build srcs/${service} -t "${service}-image" > /dev/null
+printf "status - OK\n"
+done
 
-kubectl apply -f srcs/minikube_confs/wordpress.yaml
-kubectl apply -f srcs/minikube_confs/mysql.yaml
-#kubectl apply -f srcs/minikube_confs/phpmyadmin.yaml
-#kubectl apply -f srcs/minikube_confs/nginx.yaml
+kubectl apply -f srcs/minikube_confs/volume.yaml
 
+for service in "${services[@]}"
+do
+kubectl apply -f srcs/minikube_confs/${service}.yaml
+done
+
+printf "\n"
+
+printf "Ip address nginx: 192.168.99.101:443\n"
+printf "Ip address wordpress: 192.168.99.102:5050\n"
+printf "Ip address phpmyadmin: 192.168.99.103:5000\n"
+printf "Ip address grafana: 192.168.99.104:3000\n"
+
+printf "\n"
+
+printf "URL influx: http://influxdb-svc:8086\n"
+printf "URL mysql: http://mysql-svc:3306\n"
