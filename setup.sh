17a3d78c391073b3dc8950d6f851263cf03708bb
Irving Micah <imicah@va-g1.kzn.21-school.ru>
2020-09-13 21:31:53 +0300

    complited

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-services=("nginx" "phpmyadmin" "wordpress" "mysql" "influx" "grafana", "ftps")
+services=("nginx" "phpmyadmin" "wordpress" "mysql" "influx" "grafana" "ftps")
 
 minikube start --vm-driver=virtualbox
 eval $(minikube docker-env) > /dev/null
@@ -10,12 +10,13 @@ kubectl apply -f srcs/minikube_confs/metallb-configmap.yaml
 
 for service in "${services[@]}"
 do
-printf "docker build ${service}: "
-if docker build srcs/${service} -t "${service}-image" > /dev/null 2 > error.log
-then 
-echo -en "status - \033[1m\033[32mOK\n"
+echo -en "\033[1mdocker build ${service}: "
+if docker build srcs/${service} -t "${service}-image" 1> defult.log 2> error.log
+then
+echo -en "status - \033[32m OK \033[0m \033[1m \n"
 else
-echo -en "status - \033[1m\033[31mKO\n"
+echo -en "status - \033[31m KO \033[0m \033[1m \n"
+fi
 done
 
 kubectl apply -f srcs/minikube_confs/volume.yaml
@@ -27,13 +28,13 @@ done
 
 echo -en "\n"
 
-echo -en "Ip address nginx: \033[1m\192.168.99.101:443\n"
-echo -en "Ip address wordpress: \033[1m\192.168.99.102:5050\n"
-echo -en "Ip address phpmyadmin: \033[1m\192.168.99.103:5000\n"
-echo -en "Ip address grafana: \033[1m\192.168.99.104:3000\n"
-echo -en "Ip address ftps: \033[1m\192.168.99.105:21\n"
+echo -en "Ip address nginx: 192.168.99.101:443\n"
+echo -en "Ip address wordpress: 192.168.99.102:5050\n"
+echo -en "Ip address phpmyadmin: 192.168.99.103:5000\n"
+echo -en "Ip address grafana: 192.168.99.104:3000\n"
+echo -en "Ip address ftps: 192.168.99.105:21\n"
 
 echo -en "\n"
 
-echo -en "Hostname influx: \033[1m\http://influxdb-svc:8086\n"
-echo -en "Hostname mysql: \033[1m\mysql-svc:3306\n"
+echo -en "Hostname influx: http://influxdb-svc:8086\n"
+echo -en "Hostname mysql: mysql-svc:3306\n\033[0m"
